932d9f9f6627af6359e5219f868aa34a22bc08d0
DeFranS325 <sansei325@gmail.com>
2022-07-27 15:35:21 +0400

    feat: add function reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    if (n < 10)
+        return n;
+    if (n<100)
+        return ((n%10)+','+reverse(Math.trunc(n/10))).split(',').join('');
+    else{
+        return ((n%10)+','+reverse(Math.trunc(n/10))).split(',').join('');
+    }
 }
